2b648e8df88432211de506dcadcda66b020ff582
swh <shengweihao@aodainsoft.com>
2021-09-03 17:51:36 +0800

    main

--- nacos.go
+++ nacos.go
@@ -21,6 +21,10 @@ var (
 	configClient config_client.IConfigClient
 )
 
+func main()  {
+	
+}
+
 func Setup(nacosIp string, nacosPort uint64, appIp string, appPort uint64, serverName string) {
 	serverConfigs = []constant.ServerConfig{
 		{
